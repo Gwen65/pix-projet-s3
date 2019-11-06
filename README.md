45714f9bbbfd4c85fd126051187e83b810de1e8c
Gwen65 <56821280+Gwen65@users.noreply.github.com>
2019-11-06 12:54:28 +0100

    Ajout popups et correction boutonRetourMenu
    Ajout des popups explication histoire au début du jeu (phone html)
    Correction bug boutonRetourMenu sur page phone html

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # projet-pix
-Projet Pix - AMINJ GAME M2
-- Site accessible : https://gwen65.github.io/pix-projet-s3/
+Projet Pix - AMINJ GAME M1
---
+++ assets/popup.js
@@ -0,0 +1,41 @@
+$(function () {
+    "use strict";
+    $(".show").fadeIn();
+    $("span, .overlay").click(function () {
+        $(".show").fadeOut();
+    });
+});
+
+
+var images = [
+   "assets/images/popup1.png", 
+   "assets/images/popup2.png", 
+   "assets/images/popup3.png"
+];
+var num = 0;
+
+
+function next() {
+  var slider = document.getElementById("slider");
+  num++;
+  if ( num >= images.length-1 ) {
+    document.getElementById("btnNext").style.visibility="hidden";
+  }
+  slider.src = images[num];
+  document.getElementById("btnPrev").style.visibility="visible";
+}
+
+function prev() {
+  var slider = document.getElementById("slider");
+  num--;
+  if ( num == 0 ) {
+    document.getElementById("btnPrev").style.visibility="hidden";
+  }
+  if ( num != images.length-1 ) {
+    document.getElementById("btnNext").style.visibility="visible";
+  }
+  if ( num < 0 ) {
+    num = images.length-1;
+  }
+  slider.src = images[num];
+}
